48a60da64a89ae90956853ea181d279d95e455d1
Mwenje <mwenje24@gmail.com>
2022-11-26 17:06:33 +0300

    added access methods to the event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,48 @@
 class EventsController < ApplicationController
+
+    def index
+        render json: Event.all
+    end
+
+    def show
+        event = find_event
+        render json: event
+    end
+
+    def create
+        render json: Event.create(event_params), status: :created
+    end
+
+    def update
+        event = find_event
+        event.update(
+            title: params[:title], 
+            category_id: params[:category_id], 
+            location: params[:location], 
+            date: params[:date], 
+            time: params[:time], 
+            owner_id: params[:owner_id], 
+            description: params[:description], 
+            fee: params[:fee], 
+            image: params[:image]
+        )
+        render json: event, status: :accepted
+      end
+
+    def destroy
+        event = find_event
+        event.destroy
+        render json: {}, status: :ok
+      end
+
+    private
+
+    def find_event
+        Event.find_by(id: params[:id])
+    end
+
+    def event_params
+        params.permit(:title, :category_id, :location, :date, :time, :owner_id, :description, :fee, :image)
+    end
+
 end
---
+++ db/schema.rb
@@ -0,0 +1,61 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_11_26_130245) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "attendees", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "event_attendees", force: :cascade do |t|
+    t.integer "event_id"
+    t.integer "attendee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "event_categories", force: :cascade do |t|
+    t.string "category_name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.integer "category_id"
+    t.string "location"
+    t.date "date"
+    t.time "time"
+    t.integer "owner_id"
+    t.text "description"
+    t.integer "fee"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "organizers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
