6f55f3a68e2642f025df827edf573bfa0efd52c6
Mwenje <mwenje24@gmail.com>
2022-11-26 16:00:29 +0300

    created an attendee resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :attendees
   resources :organizers
   resources :event_categories
   resources :events
---
+++ db/migrate/20221126130015_create_attendees.rb
@@ -0,0 +1,11 @@
+class CreateAttendees < ActiveRecord::Migration[7.0]
+  def change
+    create_table :attendees do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
