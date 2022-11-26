89a05afd71ee66f52c74f573399cf6e24a84a96d
Mwenje <mwenje24@gmail.com>
2022-11-26 17:43:10 +0300

    added access methods to the event-category controller

--- app/controllers/event_categories_controller.rb
+++ app/controllers/event_categories_controller.rb
@@ -1,2 +1,39 @@
 class EventCategoriesController < ApplicationController
+    def index
+        render json: EventCategory.all
+    end
+
+    def show
+        event_category = find_event_category
+        render json: event_category
+    end
+
+    def create
+        render json: EventCategory.create(event_category_params), status: :created
+    end
+
+    def update
+        event_category = find_event_category
+        event_category.update(
+            category_name: params[:category_name], 
+            description: params[:description]
+        )
+        render json: event_category, status: :accepted
+      end
+
+    def destroy
+        event_category = find_event_category
+        event_category.destroy
+        render json: {}, status: :ok
+      end
+
+    private
+
+    def find_event_category
+        EventCategory.find_by(id: params[:id])
+    end
+
+    def event_category_params
+        params.permit(:category_name, :description)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# {
+#     "title": "what IOT really means", 
+#     "category_id": 1, 
+#     "location": "diani town hall", 
+#     "date": "2022-11-10", 
+#     "time": "0:0:0", 
+#     "owner_id": "malik", 
+#     "description": "we want to drive change", 
+#     "fee": 0, 
+#     "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Internet_of_Things.jpg/1200px-Internet_of_Things.jpg"
+# }
+
+# {
+#     "id": 1,
+#     "category_name": "education",
+#     "description": "This category covers educational matters",
+#     "created_at": "2022-11-26T14:39:02.314Z",
+#     "updated_at": "2022-11-26T14:39:02.314Z"
+# }
